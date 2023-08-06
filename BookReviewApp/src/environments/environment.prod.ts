e628f696ba4a7252880e09c0a706b4c52925fa1b
Son Phan <son_phan@epam.com>
2023-08-06 13:16:22 +0700

    Added footer

---
+++ BookReviewApp/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://portfolio.com/api',
+  // Other production environment variables if you have
+};
---
+++ BookReviewApp/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:4200/api',
+    // Other production environment variables if you have
+};
